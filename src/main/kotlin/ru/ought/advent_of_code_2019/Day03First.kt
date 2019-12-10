b6120756c5b936fd4b995655d0034dc7a5efd6d6
StragaSevera <ought@yandex.ru>
2019-12-10 03:13:27 +0300

    Solve first part of the third test

---
+++ src/main/kotlin/ru/ought/advent_of_code_2019/Day03First.kt
@@ -0,0 +1,92 @@
+package ru.ought.advent_of_code_2019
+
+import kotlin.math.abs
+
+data class Point(val x: Int, val y: Int) {
+    fun move(vector: String): Point {
+        val (direction, distance) = vector[0] to vector.drop(1).toInt()
+        return when (direction) {
+            'R' -> Point(x + distance, y)
+            'L' -> Point(x - distance, y)
+            'U' -> Point(x, y + distance)
+            'D' -> Point(x, y - distance)
+            else -> throw IllegalArgumentException()
+        }
+    }
+
+    operator fun plus(p: Point): Point = Point(x + p.x, y + p.y)
+    operator fun minus(p: Point): Point = Point(x - p.x, y - p.y)
+
+    val dist: Int
+        get() = abs(x) + abs(y)
+
+}
+
+data class Segment(val p1: Point, val p2: Point) {
+    private val isVertical: Boolean
+        get() = p2.x - p1.x == 0
+    private val isHorizontal: Boolean
+        get() = p2.y - p1.y == 0
+
+    private val xRange: IntRange
+        get() = minOf(p1.x, p2.x)..maxOf(p1.x, p2.x)
+    private val yRange: IntRange
+        get() = minOf(p1.y, p2.y)..maxOf(p1.y, p2.y)
+
+    init {
+        if (!isVertical && !isHorizontal)
+            throw IllegalArgumentException()
+    }
+
+    fun getIntersection(segment: Segment): Point? {
+        if ((isVertical && segment.isVertical) ||
+            (isHorizontal && segment.isHorizontal)
+        ) {
+            // Don't know how to handle intersecting when parallel
+            return null
+        }
+
+        val horizontal: Segment
+        val vertical: Segment
+        if (isHorizontal) {
+            horizontal = this
+            vertical = segment
+        } else {
+            horizontal = segment
+            vertical = this
+        }
+
+        if ((horizontal.p1.y in vertical.yRange) &&
+            (vertical.p1.x in horizontal.xRange)
+        ) {
+            val result = Point(vertical.p1.x, horizontal.p1.y)
+            if (result.x != 0 || result.y != 0)
+                return result
+        }
+        return null
+
+    }
+}
+
+object Day03First {
+    fun nearestIntersectionDistance(firstStr: String, secondStr: String): Int {
+        val first = firstStr.split(',').toWires()
+        val second = secondStr.split(',').toWires()
+        require(first.isNotEmpty())
+        require(second.isNotEmpty())
+        return first.map { a ->
+            second.map { b ->
+                a.getIntersection(b)?.dist ?: Int.MAX_VALUE
+            }.min()!!
+        }.min()!!
+    }
+
+
+    private fun List<String>.toWires(): List<Segment> {
+        var currentPoint = Point(0, 0)
+        return map { str ->
+            Segment(currentPoint, currentPoint.move(str).also { currentPoint = it })
+        }
+    }
+
+}
---
+++ src/test/kotlin/ru/ought/advent_of_code_2019/Day03FirstTest.kt
@@ -0,0 +1,23 @@
+package ru.ought.advent_of_code_2019
+
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import ch.tutteli.atrium.api.fluent.en_GB.*
+import ch.tutteli.atrium.api.verbs.expect
+
+
+object Day03FirstTest : Spek({
+    describe("Day 03 First Task") {
+        listOf(
+            "R8,U5,L5,D3" to "U7,R6,D4,L4" to 6,
+            "R75,D30,R83,U83,L12,D49,R71,U7,L72" to "U62,R66,U55,R34,D71,R55,D58,R83" to 159,
+            "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51" to "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7" to 135
+        ).forEach {
+            it(it.toString()) {
+                val (wires, result) = it
+                val distance = Day03First.nearestIntersectionDistance(wires.first, wires.second)
+                expect(distance).toBe(result)
+            }
+        }
+    }
+})
